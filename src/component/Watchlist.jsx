d39a72371588f24eb3fa28559d3fbd0cb50119cd
amoghvarshith <amoghvarshith3@gmail.com>
2024-06-25 10:28:21 +0530

    changes the button color

--- src/component/Watchlist.jsx
+++ src/component/Watchlist.jsx
@@ -148,7 +148,7 @@ function Watchlist({ watchlist, handleRemoveFromWatchList }) {
       </div>
 
       {showNotification && (
-        <div className='fixed top-0 right-0 mt-4 mr-4 bg--500 text-white p-4 rounded shadow-lg'>
+        <div className='fixed top-0 right-0 mt-4 mr-4 bg-gray-500 text-white p-4 rounded shadow-lg'>
         Deleted Sucessfully
         </div>
       )}
